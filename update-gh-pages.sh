ab25fb318570c9e9872b185530d53501938ed862
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-01-07 09:11:45 +0000

    🔨 Update deploy

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-git clone "https://github.com/Yqnn/svg-path-editor.git" .svg-path-editor-master
-cd .svg-path-editor-master
-npm install
-npm run build-prod
+git clone "https://github.com/zkrew-red/svg-path-editor.git" .svg-path-editor-main
+cd .svg-path-editor-main
+pnpm install
+pnpm run build-prod
 git checkout gh-pages
 rm -rf *.ico *.html *.js *.css *.txt assets
 mv dist/svg-path-editor/* .
 git add --all
-git commit -m "Refreshed gh-pages from master"
+git commit -m "Refreshed gh-pages from main"
 git show --stat
 
 read -p "Confirm publication? [yn] " -r
@@ -23,4 +23,4 @@ fi
 echo
 
 cd ..
-rm -rf .svg-path-editor-master
+rm -rf .svg-path-editor-main
